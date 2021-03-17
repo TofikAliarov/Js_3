54752942258c2a039c5d23137ad440c55a3a1942
Tofik Aliarov <33291319+TofikAliarov@users.noreply.github.com>
2021-03-17 10:03:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+Array.prototype.myFilter = function (callback) {
+  let filteredArr = [];
+  for (let i = 0; i < this.length; i++) {
+    const el = this[i];
+    if (!callback(el, i, this)) {
+      filteredArr.push(el);
+    }
+  }
+
+  return filteredArr;
+};
+
+function createDebounceFunction(func, ms) {
+  let timeout;
+
+  return function executedFunction() {
+    const context = this;
+    const args = arguments;
+
+    const later = function () {
+      timeout = null;
+      func.apply(context, args);
+    };
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, ms);
+  };
+}
